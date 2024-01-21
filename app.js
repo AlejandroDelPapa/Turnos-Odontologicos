9539bb16c60f8afe3c71f6c5f58cc2b57e8bfb75
Reyes-Joaquin <137961581+Reyes-Joaquin@users.noreply.github.com>
2024-01-21 09:44:18 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+// src/App.js
+import React from 'react';
+import './App.css'; // Puedes mantener o personalizar tus estilos aquí
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>Banco de pacientes, frente odontológico estudiantil</h1>
+      </header>
+      <main>
+        <button>Pacientes</button>
+        <button>Alumnos</button>
+      </main>
+    </div>
+  );
+}
+
+export default App;
