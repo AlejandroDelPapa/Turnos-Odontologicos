44e028286d2e597252ce8ee3d139d6d47a5349b6
Alejandro Del Papa <alejandro.delpapa@manomano.com>
2024-01-23 09:58:19 +0100

    Add NextUI components library + base styles

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import Link from 'next/link'
+import {Button} from "@nextui-org/button";
+import { useRouter } from 'next/router'
 
 function App() {
+  const router = useRouter()
+
+  function navigateToAlumnos() {
+    router.push('/alumnos')
+  }
+
   return (
     <div>
       <header>
-        <h1>Banco de pacientes, frente odontológico estudiantil</h1>
+        <h1 className="text-3xl font-bold underline">
+          Banco de pacientes, frente odontológico estudiantil
+        </h1>
       </header>
       <main>
         <div>
           <Link href="/pacientes">Pacientes</Link>
         </div>
         <div>
-          <Link href="/alumnos">Alumnos</Link>
+          <Button color='primary' onClick={navigateToAlumnos}>Alumnos</Button>
         </div>
       </main>
     </div>
