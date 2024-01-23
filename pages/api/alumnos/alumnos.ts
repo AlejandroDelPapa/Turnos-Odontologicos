a4d7dee57006fe261b6879507bf4bea2e0aa6612
Alejandro Del Papa <alejandro.delpapa@manomano.com>
2024-01-23 19:02:45 +0100

    Database connection

--- pages/api/alumnos/alumnos.ts
+++ pages/api/alumnos/alumnos.ts
@@ -1,25 +1,18 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import type { Alumno } from './types';
+import { sql } from "@vercel/postgres";
  
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Alumno[]>
 ) {
   if (req.method === 'GET') {
-    // the setTimeout is just to simulate an async request with 2 seconds delay
-    setTimeout(() => {
-      res.status(200).json([
-        {
-          id: 1,
-          firstName: 'Juan',
-          lastName: 'Perez',
-        },
-        {
-            id: 2,
-            firstName: 'Ana',
-            lastName: 'Garcia',
-          }
-      ])
-    }, 2000);
+    const { rows } = await sql`SELECT * from alumno`;
+
+    res.status(200).json(rows.map((row) => ({
+      id: row.id,
+      firstName: row.first_name,
+      lastName: row.last_name,
+    })));
   }
 }
